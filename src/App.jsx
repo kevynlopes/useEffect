193b37c4db1f73f065c04c4746699c7e5f6e00d3
Kevyn Lopes <kevynlopes2019@gmail.com>
2023-07-19 23:02:39 -0300

    useEffect

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+import Produto from "./Produto";
+
+const App = () => {
+  return <div></div>;
+};
+
+export default App;
---
+++ src/Produto.jsx
@@ -0,0 +1,5 @@
+const Produto = () => {
+  return <h1>Meu produto</h1>;
+};
+
+export default Produto;
