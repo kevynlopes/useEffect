d98727fd8d9997f8bed3649ef2622828c1a82881
Kevyn Lopes <kevynlopes2019@gmail.com>
2023-07-21 12:38:43 -0300

    End: useEffect challenge

--- src/Produto.jsx
+++ src/Produto.jsx
@@ -2,19 +2,19 @@ import { useEffect, useState } from "react";
 
 const Produto = ({ produto }) => {
   const [dados, setDados] = useState(null);
-
   useEffect(() => {
-    fetch(`https://ranekapi.origamid.dev/json/api/produto/${produto}`)
-      .then((response) => response.json())
-      .then((json) => setDados(json));
-  });
-
+    if (produto !== null)
+      fetch(`https://ranekapi.origamid.dev/json/api/produto/${produto}`)
+        .then((response) => response.json())
+        .then((json) => setDados(json));
+  }, [produto]);
+  if (dados === null) return null;
   return (
     <div>
       {dados && (
         <div>
           <h1>{dados.nome}</h1>
-          <p>{dados.preco}</p>
+          <p>R$ {dados.preco}</p>
         </div>
       )}
     </div>
